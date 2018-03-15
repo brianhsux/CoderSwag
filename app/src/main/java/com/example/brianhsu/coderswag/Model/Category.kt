48cda6d5b4a841ab4dae570815bac8cad9f83d63
Brian Hsu <brianhsux@gmail.com>
2018-03-16 01:23:53 +0800

    lesson-02

--- app/src/main/java/com/example/brianhsu/coderswag/Model/Category.kt
+++ app/src/main/java/com/example/brianhsu/coderswag/Model/Category.kt
@@ -3,4 +3,8 @@ package com.example.brianhsu.coderswag.Model
 /**
  * Created by brianhsu on 8/21/17.
  */
-class Category(val title: String, val image: String)
+class Category(val title: String, val image: String) {
+    override fun toString(): String {
+        return title
+    }
+}
